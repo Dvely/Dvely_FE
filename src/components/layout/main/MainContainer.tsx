b5ff499734e695b8c2482ff61fc48b652c347c85
moonch <cogus2010@naver.com>
2026-04-19 14:58:12 +0900

    feat: PainPointsSection섹션 작업업

--- src/components/layout/main/MainContainer.tsx
+++ src/components/layout/main/MainContainer.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./HeroSection"
 import HighlightSection from "./HighlightSection"
+import PainPointsSection from "./PainPointsSection"
 
 function MainContainer() {
   return (
     <main className="w-[1440px] mx-auto flex flex-col">
         <HeroSection />
         <HighlightSection />
+        <PainPointsSection />
     </main>
   )
 }
