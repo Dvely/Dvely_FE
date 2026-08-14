8ded7daae6e2d90f8b9c60c21f05e63166e394be
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-08-14 19:38:18 +0900

    chore : LocatorJS 개발 환경 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    babel({
+      plugins: [['@locator/babel-jsx/dist', { env: 'development' }]],
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
